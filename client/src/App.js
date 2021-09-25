14527a9973ffcf08f8fab47ecb46835964007fac
Khalid Abbas <khalid.abbas.iu@gmail.com>
2021-09-26 01:43:04 +0500

    Header designing...

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
 import MainLayout from './components/layouts/MainLayout';
+import './App.css';
 
 const App = () => {
     return(
-        <div>
+        <div className="td-app-container">
             <Header/>
            <MainLayout/>
         </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return(
-        <div>
-          Header
+        <div className="td-header">
+          <img src="./images/temp-logo.svg" height="40px"/>
+          <span style={{fontWeight:'bold',color:'green'}}>TEXT DESIGNER</span>
+          <div className="td-center-element">
+            <img src="./images/avator.jpg" height="34px"/>
+            <span style={{marginLeft:'7px'}}>Abbas</span>
+          </div>
+          
         </div>
     )
 }
