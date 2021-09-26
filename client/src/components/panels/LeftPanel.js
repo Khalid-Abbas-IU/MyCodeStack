ecd97199a8050b121bf848719410be4cd9a15dbe
Zawwar <zawwarmehboob56@gmail.com>
2021-09-26 22:10:02 +0500

    added arrow animation

--- client/src/components/panels/LeftPanel.js
+++ client/src/components/panels/LeftPanel.js
@@ -3,14 +3,14 @@ import React, {useState} from 'react';
 const LeftPanel = () => {
 
  const   [isModalOpen,setModal] = useState(false);
-
-    const openLeftSection = ()=>{
+ const openLeftSection = () =>{
         setModal(!isModalOpen);
     }
 
     return(
         <div className={`td-left-panel td-center-element ${isModalOpen ? 'show-modal' :''}`}>
-                <button className="open-cta" onClick={openLeftSection}>+
+                <button className="open-cta" onClick={openLeftSection}>
+                    <img src="images/right.svg"/>
                 </button>
             <div className="content-wrapper">
                 <button className="cta-button">
@@ -19,7 +19,6 @@ const LeftPanel = () => {
             </div>
         </div>
     )
-
 }
 
 export default LeftPanel;
