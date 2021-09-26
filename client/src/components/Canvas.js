58fda525ee34253314e83dbdf1dd83efdeb8e998
Khalid Abbas <khalid.abbas.iu@gmail.com>
2021-09-26 18:06:26 +0500

    set Canvas and active object

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -17,52 +17,34 @@ class Canvas extends Component {
         }
     }
     componentDidMount(){
-        let canvas = new fabric.Canvas('canvas',{
+        this.initCanvas();
+    }
+
+    initCanvas = () => {
+        this.canvas = new fabric.Canvas('canvas', {
             height:500,
             width:900,
             backgroundColor:"#fff",
-            selection: true
-        })
-        window.canvas = canvas;
-        this.props.setCanvas(canvas);
-        canvas.on({
-            // 'object:modified':this.canvasEvents.onObjectModified,
-            'object:selected':this.onObjectSelected,
-            // 'object:removed':this.canvasEvents.objectRemoved,
-            // 'object:added':this.canvasEvents.objectAdded,
-            // 'object:scaled':this.onObjectScaled,
-            // 'object:moved': this.canvasEvents.onObjectMoved,
-            // 'object:scaling':this.onObjectScaling,
-            // 'selection:created': this.onObjectSelected,
-            // 'mouse:up:before': this.canvasEvents.onMouseUp,
-            // 'selection:updated':this.objectSelectionUpdated,
-            'selection:cleared':this.objectSelectionCleared,
-            // 'mouse:up': this.onMouseUp,
-            // 'object:moving': this.selectMovingObject,
-            // 'text:changed':this.canvasEvents.textUpdated,
-            // 'after:render':this.canvasEvents.afterRender,
-        })
-
-        let rect = new fabric.Rect({
-            top : 100,
-            left : 100,
-            width : 60,
-            height : 70,
-            fill : 'blue'
+            selection: true,
+            preserveObjectStacking: true,
         });
-        var textbox = new fabric.Textbox('Text Designer', {
+        window.canvas = this.canvas;
+        this.props.setCanvas(this.canvas);
+        let textBox = new fabric.Textbox('Text Designer', {
             left: 50,
             top: 50,
             width: 120,
             fontSize: 20
-          });
-          canvas.add(textbox).setActiveObject(textbox);
-
-        canvas.add(rect);
+        });
+        this.canvas.add(textBox).setActiveObject(textBox);
+        this.canvas.on({
+            'object:selected':this.onObjectSelected,
+            'selection:cleared':this.objectSelectionCleared,
+        })
     }
 
     onObjectSelected = (e) =>{
-        console.log("e.target.type :",e.target.type);
+        console.log("e.target.type :",e);
         if(e.target.type === 'activeSelection') return;
         this.props.setActiveObject(e.target);
 
--- client/src/components/layouts/MainLayout.js
+++ client/src/components/layouts/MainLayout.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LeftPanel from '../panels/LeftPanel';
 import RightPanel from '../panels/RightPanel';
 import MainPanel from '../panels/MainPanel';
+import {connect} from "react-redux";
 
-const MainLayout=()=>{
+const MainLayout=(props)=>{
+    const [activeObject, setActiveObject]=useState(false)
+
+    useEffect(()=>{
+        if (props.canvas && props.canvas.getActiveObject()){
+            console.log("props.canvas.getActiveObject()",props.canvas.getActiveObject())
+            setActiveObject(true)
+        }
+
+    },[])
+
+
     return(
         <div className="td-main-layout">
             <LeftPanel/>
             <MainPanel/>
-            <RightPanel/>
+            {
+                activeObject && <RightPanel/>
+            }
         </div>
     );
 
 }
+const mapStateToProps = (state) =>{
+    return{
+        canvas:state.canvas
+    }
+}
 
-export default MainLayout;
+export default connect(mapStateToProps,null)(MainLayout);
