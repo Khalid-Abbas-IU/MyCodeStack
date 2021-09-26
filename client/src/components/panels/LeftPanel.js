72cb9700f969eeff14ad0eb9f1a1c0e07bd1ae64
Zawwar <zawwarmehboob56@gmail.com>
2021-09-26 18:55:07 +0500

    left panel

--- client/src/components/panels/LeftPanel.js
+++ client/src/components/panels/LeftPanel.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const LeftPanel = () => {
+
+ const   [isModalOpen,setModal] = useState(false);
+
+    const openLeftSection = ()=>{
+        setModal(!isModalOpen);
+    }
+
     return(
-        <div className="td-left-panel td-center-element">
-          LeftPanel
+        <div className={`td-left-panel td-center-element ${isModalOpen ? 'show-modal' :''}`}>
+                <button className="open-cta" onClick={openLeftSection}>+
+                </button>
+            <div className="content-wrapper">
+                <button className="cta-button">
+                    <img src = "images/text.svg"/>
+                </button>
+            </div>
         </div>
     )
+
 }
 
 export default LeftPanel;
